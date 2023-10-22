e7c2702088c3538051c24ae9e0d9de97e53019a6
Eddyconnect <106748705+Eddyconnect@users.noreply.github.com>
2023-10-22 10:41:10 +0100

    movie card project

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
+
+const API_URL = "http://www.omdbapi.com?apikey=79a4d870";
+
+const movie1 = {
+  Title: "Titanic 666",
+  Year: "2022",
+  imdbID: "tt18394428",
+  Type: "movie",
+  Poster: "N/A",
+};
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchMovie = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovie("Extraction");
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>MovieHub</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovie(searchTerm)}
+        />
+      </div>
+      {movies.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movie found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+export default App;
